6cc3aa1e51d584f810ecde1a018550207293bd3e
aconfee <adamestela@gmail.com>
2016-05-29 17:10:03 -0500

    Placeholder data moved from view to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,79 @@
 module.exports.homelist = function(req, res){
-  res.render('locations-list', {title: 'Home'});
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'find places to work with wifi near you!'
+    },
+    sidebar: "Loc8r helps you find places to work when out and about. hizzah",
+    locations: [
+      {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },
+      {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Premium wifi'],
+        distance: '200m'
+      }
+    ]
+  });
 };
 
 module.exports.locationInfo = function(req, res){
-  res.render('location-info', {title: 'Location Info'});
+  res.render('location-info', {
+    title: 'Starcups',
+    pageHeader: {title: 'Starcups'},
+    sidebar:{
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location:
+    {
+      name: 'Starcups',
+      address: '125 High Streed, REading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot rinks', 'Food', 'Premium wifi'],
+      coords: {lat: 51.455041, lng: -0.9690884},
+      openingTimes:[
+        {
+          days: 'Monday - Friday',
+          opening: '7:00am',
+          closing: '7:00pm',
+          closed: false
+        },
+        {
+          days: 'Saturday',
+          opening: '8:00am',
+          closing: '5:00pm',
+          closed: false
+        },
+        {
+          days: 'Sunday',
+          closed: true
+        }
+      ],
+      reviews: [
+        {
+          author: 'Simon Holmes',
+          rating: 5,
+          timestamp: '16 July 2013',
+          reviewText: 'What a great place.'
+        },
+        {
+          author: 'Charlie Chaplin',
+          rating: 3,
+          timestamp: '16 June 2013',
+          reviewText: 'What a not great place.'
+        }
+      ]
+    }
+  });
 };
 
 module.exports.addReview = function(req, res){
